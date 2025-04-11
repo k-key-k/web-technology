d8d7ed1a433e75c6e9e63e6041a69ba11ac6decc
k-key-k <vaizat01@gmail.com>
2025-04-11 10:23:29 +0300

    Реализован паттерн наблюдатель для отслеживания новых задач

--- src/model/task-model.js
+++ src/model/task-model.js
@@ -1,10 +1,34 @@
 import { tasks } from "../mock/task.js";
+import { generateID } from "../utils.js";
 
 export default class TasksModel {
     #boardtasks = tasks;
+    #observers = [];
 
     get tasks() {
         return this.#boardtasks;
+    }    
+
+    addTask(title) {
+        const newTask = {
+            title,
+            status: "backlog",
+            id: generateID(),
+        };
+        this.#boardtasks.push(newTask);
+        this._notifyObserver();
+        return newTask;
     }
 
+    addObserver(observer) {
+        this.#observers.push(observer);
+    }
+
+    removeObserver(observer) {
+        this.#observers = this.#observers.filter((obs) => obs !== observer);
+    }
+
+    _notifyObserver() {
+        this.#observers.forEach((observer) => observer());
+    }
 }
