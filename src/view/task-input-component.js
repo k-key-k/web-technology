e69125933f8b613dc461056ddc9066a2be2772df
k-key-k <vaizat01@gmail.com>
2025-04-11 10:22:24 +0300

    В компонент task-input добавлен обработчик события

--- src/view/task-input-component.js
+++ src/view/task-input-component.js
@@ -6,17 +6,28 @@ function createFormAddTaskComponentTemplate() {
             <h2 class="widget__title">Новая задача</h2>
             <form class="form">
                 <label for="task-name" class="visually-hidden">Введите название задачи</label>
-                <input type="text" id="task-name" class="form__input" placeholder="Название задачи..." aria-label="Введите название задачи">
+                <input type="text" id="add-task-name" class="form__input" placeholder="Название задачи..." aria-label="Введите название задачи">
                 <button type="submit" class="form__btn" aria-label="Добавить задачу">+ Добавить</button>
             </form>
         </div>`
     );
 }
 
-export default class FormAddTaskComponent extends AbstractComponent {
-    
+export default class FormAddTaskComponent extends AbstractComponent {    
+    #handleClick = null;
+
+    constructor({onClick}) {
+        super();
+        this.#handleClick = onClick;
+        this.element.addEventListener('submit', this.#clickHandler)
+    }
+
     get template() {
         return createFormAddTaskComponentTemplate();
     }
 
+    #clickHandler = (evt) => {
+        evt.preventDefault();
+        this.#handleClick();
+    };
 }
