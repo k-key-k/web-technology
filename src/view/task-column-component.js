7763f398f1cc079f87113b213dd65c047ddd8a60
k-key-k <vaizat01@gmail.com>
2025-05-01 21:25:18 +0300

    Добавлены обработчики событий перетаскивания задач

--- src/view/task-column-component.js
+++ src/view/task-column-component.js
@@ -10,8 +10,29 @@ function createTasksListComponentTemplate() {
 
 export default class TasksListComponent extends AbstractComponent {
 
+    constructor({status, label, onTaskDrop}) {
+        super();
+        this.status = status;
+        this.label = label;
+        this.#setDropHandler(onTaskDrop);
+    }
+    
     get template() {
         return createTasksListComponentTemplate();
+    }
+
+    #setDropHandler(onTaskDrop) {
+        const container = this.element;
+
+        container.addEventListener('dragover', (event) => {
+            event.preventDefault();
+        });
+
+        container.addEventListener('drop', (event) => {
+            event.preventDefault();
+            const taskId = event.dataTransfer.getData('text/plain');
+            onTaskDrop(taskId, this.status);
+        });
     }
 
 }
