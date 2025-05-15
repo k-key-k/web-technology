7bbd114bb5f73f95a889c7c5b17dcce5d4a6cf69
k-key-k <vaizat01@gmail.com>
2025-05-15 20:35:23 +0300

    Добавлена ссылка на сервер с данными

--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import FormAddTaskComponent from './view/task-input-component.js';
 import TasksBoardPresenter from './presenter/tasks-board-presenter.js';
 import { render, RenderPosition } from './framework/render.js';
 import TasksModel from './model/task-model.js';
+import TasksApiService from './tasks-api-service.js';
 
+const END_POINT = 'https://6825ee5a397e48c913144539.mockapi.io';
 const bodyContainer = document.querySelector('.board-app');
 const formContainer = document.querySelector('.task-form');
 const tasksBoardContainer = document.querySelector('.board');
 
-const tasksModel = new TasksModel();
+const tasksModel = new TasksModel({
+    tasksApiService: new TasksApiService(END_POINT)
+});
+
 const tasksBoardPresenter = new TasksBoardPresenter({
     boardContainer: tasksBoardContainer,
     tasksModel,
